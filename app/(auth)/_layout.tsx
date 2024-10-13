d7f14d1c5fc529e4e0d045be758f060e64db0afe
Om Patel <omunite21@gmail.com>
2024-10-13 22:30:26 +0530

    Initial Commit

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,15 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="welcome" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
+}
