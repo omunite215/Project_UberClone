66429680bb642cc917822850cdc5808d0f27df5b
Om Patel <omunite21@gmail.com>
2024-10-17 23:55:47 +0530

    Editing Tab Layout

---
+++ components/RideCard.tsx
@@ -0,0 +1,11 @@
+import type { Ride } from '@/types/type'
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const RideCard = ({ ride } : {ride: Ride}) => (
+    <View>
+        <Text></Text>
+    </View>
+)
+
+export default RideCard
