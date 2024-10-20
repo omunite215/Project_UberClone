088dc59fe5f47a7f91a39e78f4a60ee78b0ad9ac
Om Patel <omunite21@gmail.com>
2024-10-20 23:53:58 +0530

    Building Home Page UI

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,5 +1,16 @@
+import GoogleTextInput from "@/components/GoogleTextInput";
+import Map from "@/components/Map";
 import RideCard from "@/components/RideCard";
-import { FlatList, Text, View } from "react-native";
+import { icons, images } from "@/constants";
+import { useUser } from "@clerk/clerk-expo";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const recentRides = [
@@ -110,6 +121,11 @@ const recentRides = [
 ];
 
 const Home = () => {
+  const { user } = useUser();
+  const loading = false;
+
+  const handleSignout = () => {};
+  const handleDestinationPress = () => {};
   return (
     <SafeAreaView className=" bg-general-500">
       <FlatList
@@ -117,6 +133,65 @@ const Home = () => {
         renderItem={({ item }) => <RideCard ride={item} />}
         className="px-5"
         keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center">
+            {!loading ? (
+              <>
+                <Image
+                  source={images.noResult}
+                  className="w-40 h-40"
+                  alt="No recent rides found"
+                  resizeMode="contain"
+                />
+                <Text className="text-sm">No recent rides found</Text>
+              </>
+            ) : (
+              <ActivityIndicator size="small" color="#00B39C" />
+            )}
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <>
+            <View className="flex flex-row items-center justify-between my-5">
+              <Text className="text-2xl capitalize font-JakartaExtraBold">
+                Welcome,{" "}
+                {user?.firstName ||
+                  user?.emailAddresses[0].emailAddress.split("@")[0]}{" "}
+                👋
+              </Text>
+              <TouchableOpacity
+                className="justify-center items-center w-10 h-10 rounded-full bg-white"
+                onPress={handleSignout}
+              >
+                <Image
+                  source={icons.out}
+                  alt="logout"
+                  className="w-4 h-4"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+            <GoogleTextInput
+              icon={icons.search}
+              containerStyle="bg-white shadow-md shadow-neutral-300"
+              handlePress={handleDestinationPress}
+            />
+            <>
+              <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                Your Current Location
+              </Text>
+              <View className="flex flex-row items-center bg-transparent h-[300px]">
+                <Map />
+              </View>
+              <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                Recent Rides
+              </Text>
+            </>
+          </>
+        )}
       />
     </SafeAreaView>
   );
