9c6221c82dc3141dc976805837218a7fe0f054f9
Om Patel <omunite21@gmail.com>
2024-10-25 22:27:53 +0530

    Completed Zustand State Mgmt.

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,8 +1,11 @@
 import GoogleTextInput from "@/components/GoogleTextInput";
+import * as Location from "expo-location";
 import Map from "@/components/Map";
 import RideCard from "@/components/RideCard";
 import { icons, images } from "@/constants";
+import { useLocationStore } from "@/store";
 import { useUser } from "@clerk/clerk-expo";
+import { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   FlatList,
@@ -121,11 +124,37 @@ const recentRides = [
 ];
 
 const Home = () => {
+  const { setUserLocation, setDestinationLocation } = useLocationStore();
   const { user } = useUser();
   const loading = false;
 
+  const [hasPremissions, setHasPermissions] = useState(false);
+
   const handleSignout = () => {};
   const handleDestinationPress = () => {};
+
+  useEffect(() => {
+    const requestLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setHasPermissions(false);
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync();
+      const address = await Location.reverseGeocodeAsync({
+        latitude: location.coords?.longitude!,
+        longitude: location.coords?.latitude!,
+      });
+      setUserLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        address: `${address[0].name}, ${address[0].region}`,
+      });
+
+    };
+    requestLocation();
+  }, []);
+
   return (
     <SafeAreaView className=" bg-general-500">
       <FlatList
