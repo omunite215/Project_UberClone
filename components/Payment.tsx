a89d8ee164995b178ba09d378aebba89bb844da2
Om Patel <omunite21@gmail.com>
2024-10-31 14:28:04 +0530

    Completed Stripe Integration

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,13 +1,96 @@
-import { View } from "react-native"
+import { Alert, View } from "react-native";
 import CustomButton from "./CustomButton";
+import { PaymentSheetError, useStripe } from "@stripe/stripe-react-native";
+import { useState, useEffect } from "react";
+import { fetchAPI } from "@/lib/fetch";
+import type { PaymentProps } from "@/types/type";
 
-const Payment = () => {
-    const openPaymentSheet = async() => {}
-    return (
-        <View>
-            <CustomButton title="Confirm Ride" className="my-10" onPress={openPaymentSheet} />
+const Payment = ({
+  fullName,
+  email,
+  amount,
+  driverId,
+  rideTime,
+}: PaymentProps) => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [success, setSuccess] = useState(false);
 
-        </View>
-    )
+  const confirmHandler = async (paymentMethod, _, intentCreationCallback) => {
+    // Make a request to your own server.
+    const { paymentIntent, customer } = await fetchAPI(
+      "/(api)/(stripe)/create",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: fullName || email.split("@")[0],
+          email: email,
+          amount: amount,
+          paymentMethodId: paymentMethod.id,
+        }),
+      }
+    );
+    if (paymentIntent.client_secret) {
+      const { result } = await fetchAPI("/(api)/(stripe)/pay", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application.json",
+        },
+        body: JSON.stringify({
+          payment_method_id: paymentMethod.id,
+          payment_intent_id: paymentIntent.id,
+          customer_id: customer,
+        }),
+      });
+      if(result.client_secret) {
+        //ride/create
+      }
+    }
+    
+    // Call the `intentCreationCallback` with your server response's client secret or error
+    const { client_secret, error } = await response.json();
+    if (client_secret) {
+      intentCreationCallback({ clientSecret: client_secret });
+    } else {
+      intentCreationCallback({ error });
+    }
+  };
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: 1099,
+          currencyCode: "USD",
+        },
+        confirmHandler: confirmHandler,
+      },
+    });
+    if (error) {
+      // handle error
+    }
+  };
+
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code} ${error.message}`);
+    } else {
+      setSuccess(true);
+    }
+  };
+  return (
+    <View>
+      <CustomButton
+        title="Confirm Ride"
+        className="my-10"
+        onPress={openPaymentSheet}
+      />
+    </View>
+  );
 };
 export default Payment;
